ae40c328410a3db8d5d28ab3f577b247d1e552c3
Guido Matos <guido.matos.88@gmail.com>
2019-10-31 10:48:50 -0500

    Fin de la sección 7

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RouterModule } from '@angular/router';
+
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { HeaderComponent } from './header/header.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
+    imports:[
+        RouterModule,
+        CommonModule
+    ],
     declarations: [
         BreadcrumbsComponent,
         HeaderComponent,
@@ -19,4 +27,6 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     ]
 })
 
-export class SharedModule { }
+export class SharedModule {
+
+}
